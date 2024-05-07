c47b4547800496f1f379c0d7051c4a229e2829fa
Eduardo Aguilar <es123y123@gmail.com>
2024-05-07 11:21:26 -0500

    cw

--- src/api/apiurls.jsx
+++ src/api/apiurls.jsx
@@ -1,7 +1,7 @@
 // URL base común
 //export const base = "http://localhost:8082"
 
-export const base = "http://http://telemetriaperu.com:8086"
+export const base = "http://telemetriaperu.com:8086"
 
 export const baseURL = `${base}/api`;
 
