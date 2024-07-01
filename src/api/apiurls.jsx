9e3ff6a864e42f04dbd19a8a17eb9cc0e2db8c5e
Eduardo Aguilar <es123y123@gmail.com>
2024-07-01 11:41:40 -0500

    ccc

--- src/api/apiurls.jsx
+++ src/api/apiurls.jsx
@@ -2,9 +2,9 @@
 //
 //export const base = "http://localhost:8088"
 
-export const base = "http://192.168.0.204:8086"
+// export const base = "http://192.168.0.204:8086"
 
-//export const base = "http://192.168.1.152:8086"
+export const base = "http://samloto.com:4000"
 
 export const baseURL = `${base}/api`;
 
